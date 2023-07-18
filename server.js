786a1d109fbd9aab696e043d6fc5d9921439a6b3
Leelaprasath <118681685+Leelaprasath@users.noreply.github.com>
2023-07-18 22:37:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const app= express();
 const cors=require('cors');
 require('dotenv').config();
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.use(cors({origin:['http://localhost:3000','https://leelaprasath.github.io/']}));
 const Adminfunc=require('./routes/admin');
-const Stafffunc=require('../chess_backend/routes/staffroutes');
-const Studentfunc=require('../chess_backend/routes/studentroutes');
+const Stafffunc=require('./routes/staffroutes');
+const Studentfunc=require('./routes/studentroutes');
 
 app.use('/api/v1/admin',Adminfunc);
 app.use('/api/v1/staff',Stafffunc);
